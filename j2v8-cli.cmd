d00c1aeae7a9a2e75bb98933af8e7c7ad493c2cb
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2022-01-24 21:05:21 +0900

    python 2.7.10

--- j2v8-cli.cmd
+++ j2v8-cli.cmd
@@ -2,6 +2,6 @@
 :: to your current command-shell instance. (can be invoked as "j2v8-cli")
 @echo off
 
-doskey build=python build.py $*
-doskey nodejs=python nodejs.py $*
-doskey citests=python build_system\run_tests.py $*
+doskey build=python2 build.py $*
+doskey nodejs=python2 nodejs.py $*
+doskey citests=python2 build_system\run_tests.py $*
